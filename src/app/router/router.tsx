edc523f1873ab00b27a4dcf8ac3b783aba57bb67
MariaSemenovaDev <semenovamk96@mail.ru>
2026-07-08 22:51:11 +0300

    config router for gh-pages

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -14,56 +14,61 @@ import { RentShelfPage } from '@/pages/public/rent-shelf-page'
 import { ShelvesPage } from '@/pages/public/shelves-page'
 import { ROUTES } from '@/shared/config/routes'
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: ROUTES.home,
+      element: <PublicLayout />,
+      errorElement: <NotFoundPage />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        },
+        {
+          path: ROUTES.catalog.slice(1),
+          element: <CatalogPage />,
+        },
+        {
+          path: ROUTES.product.slice(1),
+          element: <ProductPage />,
+        },
+        {
+          path: ROUTES.masters.slice(1),
+          element: <MastersPage />,
+        },
+        {
+          path: ROUTES.master.slice(1),
+          element: <MasterPage />,
+        },
+        {
+          path: ROUTES.shelves.slice(1),
+          element: <ShelvesPage />,
+        },
+        {
+          path: ROUTES.giftQuiz.slice(1),
+          element: <GiftQuizPage />,
+        },
+        {
+          path: ROUTES.rentShelf.slice(1),
+          element: <RentShelfPage />,
+        },
+        {
+          path: ROUTES.events.slice(1),
+          element: <EventsPage />,
+        },
+        {
+          path: ROUTES.favorites.slice(1),
+          element: <FavoritesPage />,
+        },
+      ],
+    },
+    {
+      path: '*',
+      element: <NotFoundPage />,
+    },
+  ],
   {
-    path: ROUTES.home,
-    element: <PublicLayout />,
-    errorElement: <NotFoundPage />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: ROUTES.catalog.slice(1),
-        element: <CatalogPage />,
-      },
-      {
-        path: ROUTES.product.slice(1),
-        element: <ProductPage />,
-      },
-      {
-        path: ROUTES.masters.slice(1),
-        element: <MastersPage />,
-      },
-      {
-        path: ROUTES.master.slice(1),
-        element: <MasterPage />,
-      },
-      {
-        path: ROUTES.shelves.slice(1),
-        element: <ShelvesPage />,
-      },
-      {
-        path: ROUTES.giftQuiz.slice(1),
-        element: <GiftQuizPage />,
-      },
-      {
-        path: ROUTES.rentShelf.slice(1),
-        element: <RentShelfPage />,
-      },
-      {
-        path: ROUTES.events.slice(1),
-        element: <EventsPage />,
-      },
-      {
-        path: ROUTES.favorites.slice(1),
-        element: <FavoritesPage />,
-      },
-    ],
+    basename: import.meta.env.BASE_URL,
   },
-  {
-    path: '*',
-    element: <NotFoundPage />,
-  },
-])
+)
