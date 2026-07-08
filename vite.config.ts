aea63f4f07ea16b8b2e6b99d82dc3f590ada28a1
MariaSemenovaDev <semenovamk96@mail.ru>
2026-07-08 22:45:20 +0300

    ci: deploy app to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/creative-market/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
